1ca57c05af6b8efcb6bddde87b4a464c27ddb537
freak <artback@protonmail.com>
2022-04-21 10:46:31 +0200

    Add check for having extension in database

--- pkg/repository/postgres/newconnection.go
+++ pkg/repository/postgres/newconnection.go
@@ -3,15 +3,28 @@ package postgres
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	sq "github.com/Masterminds/squirrel"
 )
 
+func HaveExtension(db *sql.DB, ctx context.Context, extName string) error {
+	stmt := sq.Select("extname").From("pg_extension").Where(sq.Eq{"extname": extName})
+	var extname string
+	if err := stmt.RunWith(db).PlaceholderFormat(sq.Dollar).QueryRowContext(ctx).Scan(&extname); err != nil {
+		return fmt.Errorf("haveExtension %w", err)
+	}
+	return nil
+}
 func NewConnection(ctx context.Context, dataSourceName string) (*sql.DB, error) {
 	db, err := sql.Open("postgres", dataSourceName)
 	if err != nil {
 		return nil, err
 	}
 	if err := db.PingContext(ctx); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("NewConnection %w", err)
+	}
+	if err := HaveExtension(db, ctx, "pg_stat_statements"); err != nil {
+		return nil, fmt.Errorf("NewConnection %w", err)
 	}
 	return db, nil
 }
---
+++ pkg/repository/postgres/newconnection_test.go
@@ -0,0 +1,50 @@
+//go:build integration
+// +build integration
+
+package postgres_test
+
+import (
+	"context"
+	"github.com/artback/hygh/pkg/repository/postgres"
+	"testing"
+)
+
+func Test_haveExtension(t *testing.T) {
+	type args struct {
+		extName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "extension that exist",
+			args: args{
+				extName: "pg_stat_statements",
+			},
+		},
+		{
+			name: "extension that don't exist",
+			args: args{
+				extName: "humble_bumble",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			db, teardown := startPG()
+			if err := postgres.HaveExtension(db, context.Background(), tt.args.extName); (err != nil) != tt.wantErr {
+				t.Errorf("haveExtension() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			err := teardown()
+			if err != nil {
+				return
+			}
+		})
+	}
+}
